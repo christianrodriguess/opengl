d39d7ab14c4aefdef8f6d822d25005bc052c9f83
Christian <christianmrodriguess@gmail.com>
2026-09-09 11:59:27 -0300

    adicionando mais entradas de teclado

--- src/main.c
+++ src/main.c
@@ -6,7 +6,10 @@ void desenhaLetraH();
 void desenhaLetraR();
 void escutaTecla(unsigned char tecla, GLint x, GLint y);
 
-GLfloat escala = 1;
+GLfloat escalaVertical = 1;
+GLfloat escalaHorizontal = 1;
+GLfloat movimentoHorizontal = 0;
+GLfloat rotacao = 0;
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
@@ -21,6 +24,62 @@ int main(int argc, char** argv) {
     glutMainLoop();
 
     return 0;
+}
+
+void desenha(void) {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    
+    gluOrtho2D(0, 12, 0, 12);
+
+    glScalef(escalaHorizontal, escalaVertical, 0);
+
+    glTranslatef(movimentoHorizontal, 0, 0);
+
+    glRotatef(rotacao, 0, 0, 1);
+    
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    desenhaLetraC();
+    desenhaLetraH();
+    desenhaLetraR();
+
+    glFlush();  
+}
+
+void escutaTecla(unsigned char tecla, GLint x, GLint y){
+    switch (tecla){
+        case '+':
+            escalaHorizontal += 0.5;
+            break;
+        case '-':
+            escalaHorizontal -= 0.5;
+            break;
+        case 'i':
+            escalaVertical += 0.5;
+            break;
+        case 'k':
+            escalaVertical -= 0.5;
+            break;
+        case 'g':
+            movimentoHorizontal--;
+            break;
+        case 'h':
+            movimentoHorizontal++;
+            break;
+        case 'n':
+            rotacao++;
+            break;
+        case 'm':
+            rotacao--;
+            break;
+        default:
+            break;
+    }
+    desenha();
 }
 
 void desenhaLetraC(){
@@ -54,38 +113,4 @@ void desenhaLetraR(){
         glVertex2f(9, 3);
         glVertex2f(11, 1);
     glEnd();
-}
-
-void desenha(void) {
-    glClear( GL_COLOR_BUFFER_BIT );
-
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    
-    gluOrtho2D(0, 12, 0, 12);
-
-    glScalef(escala, escala, 0);
-    
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-
-    desenhaLetraC();
-    desenhaLetraH();
-    desenhaLetraR();
-
-    glFlush();  
-}
-
-void escutaTecla(unsigned char tecla, GLint x, GLint y){
-    switch (tecla){
-        case '+':
-            escala++;
-            break;
-        case '-':
-            escala--;
-            break;
-        default:
-            break;
-    }
-    desenha();
 }
